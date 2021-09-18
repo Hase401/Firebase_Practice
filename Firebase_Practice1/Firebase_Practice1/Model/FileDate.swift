0716cffd178c396d74093ac7df19a2a18f1b54b5
Hase401 <ponta410422@icloud.com>
2021-09-18 13:52:23 +0900

    FileViewControllerでのデータをFirestoreと連携したものを実装

--- Firebase_Practice1/Firebase_Practice1/Model/FileDate.swift
+++ Firebase_Practice1/Firebase_Practice1/Model/FileDate.swift
@@ -16,56 +16,60 @@ import Firebase
         // createaAt(フィールド)
         // （実際には次に RoutingDays(同じようなドキュメントとサブコレクションの関係性が続く)）
 
-//final class FileDate: NSObject {
-//    var id: String
-//    var month: Int
-//    var day: Int
-//    var week: String
-//    var createAt: Date
-//
-//    init(doc: QueryDocumentSnapshot) {
-//        self.id = doc.documentID
-//
-//        let data = doc.data()
-//        self.month = data["month"] as! Int
-//        self.day = data["day"] as! Int
-//        self.week = data["week"] as! String
-//        let createAtTimestamp = data["createAt"] as? Timestamp
-//
-//        if let createAt = createAtTimestamp {
-//            self.createAt = createAt.dateValue() // Timestamp型からDate型に変換
-//        } else {
-//            // もしnilだった場合
-//            self.createAt = Date()
-//        }
-//    }
-//
-////    static func createFolderDateToFirestore(year: Int,
-////                                            month: Int) {
-////        let createTime = FieldValue.serverTimestamp()
-////        // documentはないので空のままでいい？
-////        // 【曖昧】コレクションはこれで定義して理想通りになっているか失敗しててもいいから実際にコンソールで検証するべきだな。。。
-////        Firestore.firestore().collection("folderLists/folderList/folderSections/folderSection/folderDates").document().setData([
-////            "year": year,
-////            "month": month,
-////            "createAt": createTime
-////        ],
-////        merge: true, // trueならデータがある時はupdateを行い、データがない場合はcreateを行う
-////        completion: { error in
-////            if let error = error {
-////                print("Todoの作成失敗" + error.localizedDescription)
-////            } else {
-////                print("Todoの作成成功")
-////            }
-////        })
-////    }
-////
-////    // 配列が欲しいので、yearが一致したものでmonthで並び替えたものを取ってくる？
-////    static func showFolderDateForFirestore(year: Int,
-////                                           completionAction: @escaping (QuerySnapshot?, Error?) -> Void) {
-////        Firestore.firestore().collection("folderLists/folderList/folderSections/folderSection/folderDates").whereField("year", isEqualTo: year).order(by: "month", descending: false).addSnapshotListener( {(querySnapshot, error) in
-////            completionAction(querySnapshot, error)
-////        })
-////    }
-//}
-//
+final class FileDate: NSObject {
+    var id: String
+    var year: Int
+    var month: Int
+    var day: Int
+    var week: String
+    var createAt: Date
+
+    init(doc: QueryDocumentSnapshot) {
+        self.id = doc.documentID
+
+        let data = doc.data()
+        self.year = data["year"] as? Int ?? 0
+        self.month = data["month"] as? Int ?? 0
+        self.day = data["day"] as? Int ?? 0
+        self.week = data["week"] as? String ?? ""
+        let createAtTimestamp = data["createAt"] as? Timestamp
+        if let createAt = createAtTimestamp {
+            self.createAt = createAt.dateValue() // Timestamp型からDate型に変換
+        } else {
+            self.createAt = Date()
+        }
+    }
+
+    static func createFileDateToFirestore(folderDate: FolderDate,
+                                          day: Int,
+                                          week: String,
+                                          completionAction: @escaping (Error?) -> Void) {
+        if let user = Auth.auth().currentUser {
+            let createTime = FieldValue.serverTimestamp()
+            // documentはないので空のままでいい？
+            // 【曖昧】コレクションはこれで定義して理想通りになっているか失敗しててもいいから実際にコンソールで検証するべきだな。。。
+            Firestore.firestore().collection("Users/\(user.uid)/FolderSections/folderSection/FolderDates/\(folderDate.id)/FileDates").document().setData([
+                "year": folderDate.year,
+                "month": folderDate.month,
+                "day": day,
+                "week": week,
+                "createAt": createTime
+            ],
+            merge: true, // trueならデータがある時はupdateを行い、データがない場合はcreateを行う
+            completion: { error in
+                completionAction(error)
+            })
+        }
+    }
+
+//    // 配列が欲しいので、yearが一致したものでmonthで並び替えたものを取ってくる？
+    static func showFileDateForFirestore(folderDate: FolderDate,
+                                         completionAction: @escaping (QuerySnapshot?, Error?) -> Void) {
+        if let user = Auth.auth().currentUser {
+            Firestore.firestore().collection("Users/\(user.uid)/FolderSections/folderSection/FolderDates/\(folderDate.id)/FileDates").order(by: "day", descending: true).addSnapshotListener( {(querySnapshot, error) in
+                completionAction(querySnapshot, error)
+            })
+        }
+    }
+}
+
--- Firebase_Practice1/Firebase_Practice1/Model/FolderDate.swift
+++ Firebase_Practice1/Firebase_Practice1/Model/FolderDate.swift
@@ -20,42 +20,51 @@ final class FolderDate: NSObject {
     var id: String
     var year: Int
     var month: Int
+    var totalDayInMonth: Int // 追加してみた
     var createAt: Date
+    var updateAt: Date // 追加してみた
+//    var fileDates: [FileDate] // これはボツ
 
     // folderDateは複数のドキュメントが作れれるものなのでQueryDocumentSnapshot
     init(doc: QueryDocumentSnapshot) {
         self.id = doc.documentID
 
         let data = doc.data()
-        self.year = data["year"] as! Int
-        self.month = data["month"] as! Int
+        self.year = data["year"] as? Int ?? 0
+        self.month = data["month"] as? Int ?? 0
+        self.totalDayInMonth = data["totalDayInMonth"] as? Int ?? 0
         let createAtTimestamp = data["createAt"] as? Timestamp
-
-        if let createAt = createAtTimestamp {
+        let updateAtTimestamp = data["updateAt"] as? Timestamp
+        if let createAt = createAtTimestamp,
+           let updateAt = updateAtTimestamp {
             self.createAt = createAt.dateValue() // Timestamp型からDate型に変換
+            self.updateAt = updateAt.dateValue()
         } else {
             self.createAt = Date()
+            self.updateAt = Date()
         }
+
+
     }
 
-    // 【実行】ここにFolderSectionを入れて、"\(folderSection.id)"みたいに入れてあげると良さそう？
-    static func createFolderDateToFirestore(
-                                            year: Int,
-                                            month: Int) {
+    // 【実行】ここにFolderSectionを入れて、"\(folderSection.id)"みたいに入れてあげると良さそうだけど
+        // 辞書で管理しているので上手く渡しづらい。これはしゃーない。
+        // 非同期処理のためにcompletionActionは結構必要
+    static func createFolderDateToFirestore(year: Int,
+                                            month: Int,
+                                            completionAction: @escaping (Error?) -> Void) {
         if let user = Auth.auth().currentUser {
             let createTime = FieldValue.serverTimestamp()
             Firestore.firestore().collection("Users/\(user.uid)/FolderSections/folderSection/FolderDates").document().setData([
                 "year": year,
                 "month": month,
-                "createAt": createTime
+                "totalDayInMonth": 0,
+                "createAt": createTime,
+                "updateAt": createTime
             ],
             merge: true, // trueならデータがある時はupdateを行い、データがない場合はcreateを行う
             completion: { error in
-                if let error = error {
-                    print("FolderDateの作成失敗" + error.localizedDescription)
-                } else {
-                    print("FolderDateの作成成功")
-                }
+                completionAction(error)
             })
         }
     }
@@ -64,12 +73,56 @@ final class FolderDate: NSObject {
     static func showFolderDateForFirestore(
                                            completionAction: @escaping (QuerySnapshot?, Error?) -> Void) {
         if let user = Auth.auth().currentUser {
-            // whereField("year", イコールyear)は今の所なくても大丈夫そう？？？
+            // whereField("year", イコールyear)は今の所なくても大丈夫そう
+                // 今の所, order(month)のみでいけてる
             // "users/\(user.uid)/folderSections/folderDates/folderDate"
             Firestore.firestore().collection("Users/\(user.uid)/FolderSections/folderSection/FolderDates").order(by: "month", descending: true).addSnapshotListener( {(querySnapshot, error) in
                 completionAction(querySnapshot, error)
             })
         }
     }
+
+    // これはupdateとして必要
+    static func updateTotalDayInMonth(folderDate: FolderDate,
+                                         fileDatesCount: Int,
+                                         completionAction: @escaping (Error?) -> Void) {
+        if fileDatesCount == 0 {
+            return
+        }
+        if let user = Auth.auth().currentUser {
+            // 【変更後】
+            Firestore.firestore().collection("Users/\(user.uid)/FolderSections/folderSection/FolderDates").document(folderDate.id).setData([
+                "totalDayInMonth": fileDatesCount,
+                "updateAt": FieldValue.serverTimestamp()
+            ],
+            merge: true,
+            completion: { error in
+                completionAction(error)
+            })
+            // 【変更前】
+//            Firestore.firestore().collection("Users/\(user.uid)/FolderSections/folderSection/FolderDates").document(folderDate.id).updateData([
+//                "totalDayInMonth": fileDatesCount,
+//                "updateAt": FieldValue.serverTimestamp()
+//            ], completion: { error in
+//                completionAction(error)
+//            })
+        }
+    }
+
+    // 今の所ボツ
+//    static func calculateTotalDayInYearSection(year: Int,
+//                                               folderDates: [FolderDate],
+//                                               completionAction: @escaping (Error?) -> Void) {
+//        // 共通したyearの一意な月のFileDatesをもつドキュメントたちを持ってくる
+//        if let user = Auth.auth().currentUser {
+//            Firestore.firestore().collection("Users/\(user.uid)/FolderSections/folderSection/FolderDates").whereField("year", isEqualTo: year)
+//    //            var totalDay = 0 // 初期化
+//    //            if folderDates.count != 0 {
+//    //                for i in 0...folderDates.count-1 {
+//    //                    totalDay += folderDates[i].fileDates.count
+//    //                }
+//    //            }
+//        }
+//    }
 }
 
